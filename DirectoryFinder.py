3b02113dd458045a29d93adb39cfc375706690e4
Nic Manoogian <nmanoogian@gmail.com>
2013-04-16 19:31:34 -0400

    Added wordlist and makefile. Added CL arguments

--- DirectoryFinder.py
+++ DirectoryFinder.py
@@ -1,6 +1,7 @@
 from urllib import *
+import sys
 
-file = open("words.li", "r")
+file = open(sys.argv[1], "r")
 line = file.readline()
 while line != "":
 	a=urlopen('http://www.cs.rit.edu/~vcss243/Lab/06-Micro'+line.strip())
---
+++ Makefile
@@ -0,0 +1,2 @@
+run:
+	python DirectoryFinder.py
