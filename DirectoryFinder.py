f6aca5528d01308fc9c2d863f7457559d6025b88
alkaline9500 <zimmoz3@verizon.net>
2013-04-19 09:32:55 -0400

    Added dual wordlist capability

--- DirectoryFinder.py
+++ DirectoryFinder.py
@@ -5,13 +5,27 @@ Searches for a specific file using a wordlist and header
 
 from urllib import *
 import sys
-
-file = open(sys.argv[1], "r")
-line = file.readline()
-while line != "":
-	url = sys.argv[2]+line.strip().title()
-	a=urlopen(url)
-	if a.getcode() != 404:
-		sys.stderr.write(url)
-	print(line.strip())
+if len(sys.argv) == 3:
+	file = open(sys.argv[1], "r")
 	line = file.readline()
+	while line != "":
+		url = sys.argv[2]+line.strip().title()
+		a=urlopen(url)
+		if a.getcode() != 404:
+			sys.stderr.write(url)
+		print(line.strip())
+		line = file.readline()
+else:
+	file1 = open(sys.argv[1], "r")
+	line1 = file1.readline()
+	while line1 != "":
+		file2 = open(sys.argv[2], "r")
+		line2 = file2.readline()
+		while line2 != "":
+                	url = sys.argv[3]+line1.strip().title()+"-"+line2.strip().title()
+			a=urlopen(url)
+                	if a.getcode() != 404:
+                	        sys.stderr.write(url)
+                	print(url)
+                	line2 = file2.readline()
+		line1 = file1.readline()
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 <h1>Directory Finder</h1>
 Written in Python, this script searches for a particular file or directory using a word list
+If two word lists are specified, urls will be generated with every combination
+All valid URLs are written to standard error
+<h2>Example</h2>
+apple-apple
+apple-banana
+apple-carrot
+banana-apple
+banana-banana
+banana-carrot
+...
+
+Usage: Single List
+``
+python DirectoryFinder.py [wordlist] [url prefix] 2> valid.txt
+``
+Usage: Dual List
+``
+python DirectoryFinder.py [wordlist1] [wordlist2] [url prefix] 2> valid.txt
+``
