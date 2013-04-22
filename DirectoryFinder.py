abf95395cdbca4d23f757998b199c149263e8f9f
alkaline9500 <zimmoz3@verizon.net>
2013-04-21 23:33:00 -0400

    Added documentation to DirectoryFinder.py

--- DirectoryFinder.py
+++ DirectoryFinder.py
@@ -1,10 +1,13 @@
 """
 DirectoryFinder
-Searches for a specific file using a wordlist and header
+Author: Nicolas Manoogian <njm7461@cs.rit.edu>
+Date: April 21, 2013
+Desc: Searches for a specific file using a wordlist and header
 """
 
 from urllib import *
 import sys
+# If there are only 2 command line arguments: one list
 if len(sys.argv) == 3:
 	file = open(sys.argv[1], "r")
 	line = file.readline()
@@ -12,9 +15,11 @@ if len(sys.argv) == 3:
 		url = sys.argv[2]+line.strip().title()
 		a=urlopen(url)
 		if a.getcode() != 404:
+			# Write successful URLs to Standard Err
 			sys.stderr.write(url)
 		print(line.strip())
 		line = file.readline()
+# There are 3 command line arguments: two lists
 else:
 	file1 = open(sys.argv[1], "r")
 	line1 = file1.readline()
@@ -25,6 +30,7 @@ else:
                 	url = sys.argv[3]+line1.strip().title()+"-"+line2.strip().title()
 			a=urlopen(url)
                 	if a.getcode() != 404:
+				# Write successful URLs to Standard Err
                 	        sys.stderr.write(url)
                 	print(url)
                 	line2 = file2.readline()
